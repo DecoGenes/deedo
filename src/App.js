4c2bd96fc540f4fe95bb2fd123dc026d0b3aba5c
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-22 11:50:47 +0200

    app.j updated with profile

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import DeedList from './components/Deedlist/index'
+import Profile from './components/Profile';
 
 function App() {
   return (
     <Router>
-    <Switch>
-    <div className="App">
-      <h1> testing</h1>
-    <Route path= "/DeedList">
-      <DeedList></DeedList>
-    </Route>
-    </div>
-    </Switch>   
+      <Switch>
+        <div className="App">
+          <h1> testing</h1>
+          <Route path="/DeedList">
+            <DeedList></DeedList>
+          </Route>
+          <Route path="/Profile">
+            <Profile></Profile>
+          </Route>
+        </div>
+      </Switch>
     </Router>
   );
 }
