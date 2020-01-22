98d77e7df7e397fa908d7626c94f8b29c0fd0f14
Danielle-Tal <danielle.tal1986@gmail.com>
2020-01-22 11:31:13 +0200

    Initializing css and app structre
    Initializing css and app structre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
+import DeedList from './components/Deedlist/index'
 
 function App() {
   return (
+    <Router>
+    <Switch>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> testing</h1>
+    <Route path= "/DeedList">
+      <DeedList></DeedList>
+    </Route>
     </div>
+    </Switch>   
+    </Router>
   );
 }
 
---
+++ src/components/Deedlist/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class DeedList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default DeedList
