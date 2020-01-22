7a6f2a238fe1b6c40561086d8227edaeb275bb77
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-22 18:42:14 +0200

    Update login, changing from App to Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -13,42 +13,34 @@ import firebase from './firebase'
 
 
 
-function App() {
-
-  const [login, setLogin] = useState(false)
-
-  function handleLogIn(){
-    setLogin(true)
-  }
-
+function App(props) {
   return (
-    <Router>
-      <Switch>
-        <div className="App">
-          <NavBarMain></NavBarMain>
-          <Route exact path= "/">
-            <Home />
-            <h1> testing</h1>
-            <Login onChange />
-          </Route>
-          {/* <Route exact path= "/login">
-            <Login />
-          </Route> */}
-          <Route path= "/DeedList">
-            <DeedList></DeedList>
-          </Route>
-          <Route path="/profile">
-            <Profile></Profile>
-          </Route>
-          <Route path= "/DeedDone">
-            <DeedDone></DeedDone>
-          </Route>
-          <Route path="/DeedCard">
-            <Deedcard></Deedcard>
-          </Route>
-        </div>
-      </Switch>   
-    </Router>
+
+    <Login />
+    // <Router>
+    //   <Switch>
+    //     <div className="App">
+    //       <Route exact path="/">
+            
+    //       </Route>
+    //       <Route exact path="/home">
+    //         <Home />
+    //       </Route>
+    //       <Route path="/DeedList">
+    //         <DeedList></DeedList>
+    //       </Route>
+    //       <Route path="/profile">
+    //         <Profile></Profile>
+    //       </Route>
+    //       <Route path="/DeedDone">
+    //         <DeedDone></DeedDone>
+    //       </Route>
+    //       <Route path="/DeedCard">
+    //         <Deedcard></Deedcard>
+    //       </Route>
+    //     </div>
+    //   </Switch>
+    // </Router>
   );
 }
 
