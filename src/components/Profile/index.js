ce227d044f562dd65a545af61868f858fc1203df
Andre Genes <andre.genes.vieira@gmail.com>
2020-01-22 14:52:18 +0200

    Profile component moked

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,103 +1,94 @@
 import React, { useState, useEffect } from 'react'
+import "./style.css";
 
 export default function Profile() {
 
-    // const [profile, setProfile] = useState({
-    //     deedId /*User Id */: '',
-    //     id: '',
-    //     firstName: '',
-    //     lastName: '',
-    //     birthDate: '',
-    //     email: '',
-    //     address: {
-    //         address1: '',
-    //         address2: '',
-    //         zip: '',
-    //         city: '',
-    //         country: '',
-    //     },
-    //     bloodType: '',
-    //     RhD: '',
-    //     plasmaDonor: true,
-    //     organDonor: true,
-    //     deedDone: {
-    //         category: {
-    //             blood: {
-    //                 deedDate: '',
-    //                 typeOfDeed: '',
-    //                 pointsPerDeed: '',
-    //             },
-    //         }
-    //     },
-    //     nextAvailableDeedDate: '',
-    //     deedWards: {
-    //         deedWardRetrieved: '',
-    //         deedWardDate: '',
-    //         deedWardPointUsed: '',
-    //     }
-    // })
 
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [birthDate, setBirthDate] = useState('')
+    const [deedId, setDeedId] = useState('yahenpodsandakp')
+    const [firstName, setFirstName] = useState('Andre')
+    const [lastName, setLastName] = useState('Genes')
+    const [birthDate, setBirthDate] = useState('17/07/1985')
+    const [deedBalance, setDeedBalance] = useState(0)
     const [address, setAddress] = useState({
-        address1: '',
+        address1: 'Lavie 8',
         address2: '',
-        zip: '',
-        city: '',
-        country: '',
+        zip: '5332638',
+        city: 'Givatayim',
+        country: 'Israel',
     })
     const [blood, setBlood] = useState({
-        bloodType: '',
-        RhD: '',
+        bloodType: 'AB',
+        RhD: '+',
         plasmaDonor: true
     })
-    // const [plasmaDonor, setPlasma] = useState(false)
-    const [organDonor, setOrganDonor] = useState(false)
+    const [plasmaDonor, setPlasma] = useState(false)
+    const [organDonor, setOrganDonor] = useState(true)
     const [category, setCategory] = useState([])
     const [deedDone, setDeedDone] = useState({
-        category: '',
-        deedDate: '',
+        category: 'Blood Donation',
+        deedDate: '22/01/2020',
         typeOfDeed: '',
-        pointsPerDeed: '',
+        pointsPerDeed: '5',
     })
-    const [nextAvailableDeedDate, setNextAvailableDeedDate] = useState('')
+    const [nextAvailableDeedDate, setNextAvailableDeedDate] = useState('22/04/2020')
     const [deedWards, setDeedWards] = useState({
+        deedWardsBalance: '',
         deedWardRetrieved: '',
         deedWardDate: '',
         deedWardPointUsed: '',
     })
 
+    function handlePlasmaDonor() {
+        setBlood({ plasmaDonor: true })
+    }
+
+    function handleOrganDonor() {
+        setOrganDonor(true)
+    }
+
     return (
-        <div>
-            <div>{firstName}</div>
-            <div>{lastName}</div>
-            <div>{birthDate}</div>
-            <div>
-                <div>{address.address1}</div>
-                <div>{address.address2}</div>
-                <div>{address.city}</div>
-                <div>{address.country}</div>
-                <div>{address.zip}</div>
-            </div>
-            <div>
-                <div>{blood.bloodType}</div>
-                <div>{blood.RhD}</div>
-                <div>{blood.plasmaDonor}</div>
-            </div>
-            <div>{organDonor}</div>
-            <div>
-                <div>{category}</div>
-                <div>{deedDone.category}</div>
-                <div>{deedDone.deedDate}</div>
-                <div>{deedDone.pointsPerDeed}</div>
-                <div>{deedDone.typeOfDeed}</div>
-            </div>
-            <div>{nextAvailableDeedDate}</div>
-            <div>
-                <div>{deedWards.deedWardDate}</div>
-                <div>{deedWards.deedWardPointUsed}</div>
-                <div>{deedWards.deedWardRetrieved}</div>
+        <div className='profile-page'>
+            <h1>DeedId: {deedId}</h1>
+            <div className='user-card'>
+                <div className='card'>
+                    <div>First Name: {firstName}</div>
+                    <div>Last Name: {lastName}</div>
+                    <div>Birth Date: {birthDate}</div>
+                    <div>Deed Balance: {deedBalance}</div>
+                </div>
+                <div className='card'> Address:
+                    <div>{address.address1}</div>
+                    <div>{address.address2}</div>
+                    <div>{address.city}</div>
+                    <div>{address.country}</div>
+                    <div>{address.zip}</div>
+                </div>
+                <div className='card'> Blood:
+                <span>Blood Type: {blood.bloodType}</span>
+                    <span>RhD: {blood.RhD}</span>
+                    <label>Plasma Donor:
+                <input type='checkbox' onChange={(event) => handlePlasmaDonor(event)} value={blood.plasmaDonor} />
+                    </label>
+                </div>
+                <label>Organ Donor:
+                <input type='checkbox' onChange={(event) => handleOrganDonor(event)} value={organDonor} />
+                </label>
+                <div className='card'>
+                    <div>Category: {category}</div>
+                    <div>{deedDone.category}</div>
+                    <div>Deed Date: {deedDone.deedDate}</div>
+                    <div>Points: {deedDone.pointsPerDeed}</div>
+                    <div>Deed Type: {deedDone.typeOfDeed}</div>
+                </div>
+                <div>Next Deed Date: {nextAvailableDeedDate}</div>
+                
+                <div className='card'> DeedWards
+                <div>Date: {deedWards.deedWardDate}</div>
+                    <div>DeedWard Retrieved: {deedWards.deedWardRetrieved}</div>
+                    <div>Points Used: {deedWards.deedWardPointUsed}</div>
+                </div>
+                {/* <button>Edit</button>
+                <button>Save</button> */}
             </div>
         </div>
     )
