0e5abf6c6ff5dbbf3e5a7a0153d27eef612eff82
Danielle-Tal <danielle.tal1986@gmail.com>
2020-01-22 21:32:47 +0200

    FAQ's content
    FAQ's content

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -4,27 +4,64 @@ import './style.css'
 export default class FAQ extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            showDeedo:false,
+            showBlood:false,
+            showAnimals:false,
+            showTeaching:false,
+        }
         
     }
     handleDeedoFaq(){
-
+        this.setState({showDeedo:true, showBlood:false, showAnimals:false, showTeaching:false })
+    }
+    handleBloodFaq(){
+        this.setState({showDeedo:false, showBlood:true, showAnimals:false, showTeaching:false })
+    }
+    handleAnimalsFaq(){
+        this.setState({showDeedo:false, showBlood:false, showAnimals:true, showTeaching:false })
+    }
+    handleTeachingFaq(){
+        this.setState({showDeedo:false, showBlood:false, showAnimals:false, showTeaching:true })
     }
     render() {
         return (
             <div>
                 <h2 className="faq-headline">I want to know more about:</h2>
                 <button className='btn btn-primary faq-btn' onClick={() => this.handleDeedoFaq()}>Deedo</button>
-                <button className='btn btn-primary faq-btn'>Giving blood</button>
-                <button className='btn btn-primary faq-btn'>Helping Animals</button>
-                <button className='btn btn-primary faq-btn'>Teaching others</button>
-                <div className="Deedo-faq">
+                <button className='btn btn-primary faq-btn' onClick={() => this.handleBloodFaq()}>Giving blood</button>
+                <button className='btn btn-primary faq-btn' onClick={() => this.handleAnimalsFaq()}>Helping Animals</button>
+                <button className='btn btn-primary faq-btn' onClick={() => this.handleTeachingFaq()}>Teaching others</button>
+                
+                <div className="Deedo-faq" style={{display: this.state.showDeedo ? 'block' : 'none' }}>
                     <h3>Deedo</h3>
                     <h5>Who is Deedo....</h5>
                     <p>bla bla bla</p>
                     <h5>Why should I use Deedo?</h5>
                     <p>bla bla bla</p>
                 </div>
+                <div className="Blood-faq" style={{display: this.state.showBlood ? 'block' : 'none' }}>
+                    <h3>Giving Blood</h3>
+                    <h5>How can i give blood?</h5>
+                    <p>bla bla bla</p>
+                    <h5>Where do I give blood?</h5>
+                    <p>bla bla bla</p>
+                </div>
+                <div className="Animals-faq" style={{display: this.state.showAnimals ? 'block' : 'none' }}>
+                    <h3>Helping Animals</h3>
+                    <h5>Where can i help Animals?</h5>
+                    <p>bla bla bla</p>
+                    <h5>Which Animals can i help?</h5>
+                    <p>bla bla bla</p>
+                </div>
+                <div className="Teaching-faq" style={{display: this.state.showTeaching ? 'block' : 'none' }}>
+                    <h3>Teaching</h3>
+                    <h5>What does teaching include?</h5>
+                    <p>bla bla bla</p>
+                    <h5>I don't have a teaching diploma. Can I help anyway?</h5>
+                    <p>bla bla bla</p>
+                </div>
+
             </div>
         )
     }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Nav, Navbar } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import logo from '../../images/logo.jpeg'
+import './style.css'
 
 export default function NavBarMain() {
     return (
         <div className="navbar-div">
             <Navbar bg="light" expand="lg" className="Main-navbar" >
-            <Navbar.Brand href="#home">Deedo</Navbar.Brand>
+            <Navbar.Brand href="#home"><img className="navbar-logo" src={logo}></img></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto navbar-links">
