8b112c4e15c509ff60ca52ff2586f1da1fa10e72
Danielle-Tal <danielle.tal1986@gmail.com>
2020-01-22 18:35:31 +0200

    FAQ buttons
    FAQ buttons

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import './style.css'
 
 export default class FAQ extends Component {
     render() {
         return (
             <div>
-                <h2>I want to know more about:</h2>
-                <button className='btn btn-primary'>Deedo</button>
-                <button className='btn btn-primary'>Giving blood</button>
-                <button className='btn btn-primary'>Helping Animals</button>
-                <button className='btn btn-primary'>Teaching others</button>
+                <h2 className="faq-headline">I want to know more about:</h2>
+                <button className='btn btn-primary faq-btn'>Deedo</button>
+                <button className='btn btn-primary faq-btn'>Giving blood</button>
+                <button className='btn btn-primary faq-btn'>Helping Animals</button>
+                <button className='btn btn-primary faq-btn'>Teaching others</button>
             </div>
         )
     }
