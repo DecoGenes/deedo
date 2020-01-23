a1b86b96a8c29256632f5407b38eedde8b13ed1d
janetranada <jyranada@gmail.com>
2020-01-23 11:55:13 +0200

    added media screen on deedCard

--- src/components/Deedone/index.js
+++ src/components/Deedone/index.js
@@ -32,7 +32,6 @@ const deedsHistory = [
         date: "10 January 2020",
         pointsEarned: 5,
     },
-
 ]
 
 export default class DeedDone extends React.Component {
@@ -63,28 +62,6 @@ export default class DeedDone extends React.Component {
                     )}
                 </div>
             </div>
-
-            // <div>
-            //     <h1 className="deedsdone-headline">Your Deeds History</h1>
-            //     <div className="deedsdone-card">
-            //         <p className="deedsdone-date">14 December 2019 </p>
-            //         <h3>Adop a dog for a day</h3>
-            //         <h4>Organization: Tno Lachaiot Lichyot</h4>
-            //         <p>10 points</p>
-            //     </div>
-            //     <div className="deedsdone-card">
-            //         <p className="deedsdone-date">25 December 2019 </p>
-            //         <h3>Host a soldier for a dinner</h3>
-            //         <h4>Organization: IDF</h4>
-            //         <p>5 points</p>
-            //     </div>
-            //     <div className="deedsdone-card">
-            //         <p className="deedsdone-date">10 January 2019 </p>
-            //         <h3>Young scientist</h3>
-            //         <h4>Organization: Ministry of Education</h4>
-            //         <p>10 points</p>
-            //     </div>
-            // </div>
         )
     }
 }
